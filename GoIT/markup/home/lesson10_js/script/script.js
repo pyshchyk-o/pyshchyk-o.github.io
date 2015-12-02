45b60d66dcab2b4d801b2828951c474b847a0b5a
Olya Pischik <olyapischik170496@gmail.com>
2015-12-02 08:55:54 +0200

    first js homework

--- GoIT/markup/home/lesson10_js/script/script.js
+++ GoIT/markup/home/lesson10_js/script/script.js
@@ -1,20 +1,18 @@
-function pow(x, n) {
-  var result = x;
-    
-    
-  for (var i = 1; i < n; i++){
-    result *= x;
-  }
-  return result;
-}
-var x = prompt("Укажите x", '');
-var n = prompt("Укажите n", '');
-  
-  if (n <= 1){
-      alert('Нужно больше 1');
-  }else{
-      alert(pow(x, n));
-  }
-var calResult = pow(x,n);
+var x = +prompt('Write figure: ');
+var n = +prompt('Write exponent: ');
 
-console.log('Result: ', calResult);
+
+
+function pow(x, n){
+    var result = 1;
+    for(i = 0; i < n; i++){
+        result = result * x;
+    }
+    return result;
+}
+
+var powResult = pow(x, n);
+alert(powResult);
+console.log(powResult);
+
+
