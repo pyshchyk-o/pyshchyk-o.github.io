8407fbbdd2ffe05d52bbdc0fedf93f274903a9c5
VvanGogh <olyapischik170496@gmail.com>
2016-02-18 10:53:37 +0200

    global trade

---
+++ GoIT/markup/home/Global_trade/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+
+  	grunt.initConfig({
+  	    pkg: grunt.file.readJSON('package.json'),
+
+      sass: {
+        options: {
+         sourceMap: false
+      },
+	    dist: {
+	      files: {
+	      'css/main.css':'sass/style.scss'
+	      }
+	    }
+	},
+
+      watch: {
+	    	css: {
+          files: 'sass/*.scss',
+	        tasks: ['sass']
+	    	}
+	    }
+        });
+
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'watch']);
+
+};
