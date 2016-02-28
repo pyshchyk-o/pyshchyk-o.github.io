ecf8e6c3ee533dbfa303ac67644867fce9f1d533
VvanGogh <olyapischik170496@gmail.com>
2016-02-28 10:04:56 +0200

    adding Ajax

--- GoIT/markup/home/works/AnnaK/js/script.js
+++ GoIT/markup/home/works/AnnaK/js/script.js
@@ -1,4 +1,5 @@
 $(window).load(function() {
+  // Slider
 $('.flexslider').flexslider({
     animation: "slide",
     animationLoop: false,
@@ -7,7 +8,7 @@ $('.flexslider').flexslider({
     minItems: 2,
     maxItems: 4
   });
-
+// Form - email
 var user,
     json,
     pars;
@@ -32,5 +33,43 @@ $('#form').on('submit', function(e){
   });
 });
 
+//-------------------------------------------------  Ajax - JSON - PHP
+
+//  $('#form').on('submit', function(e){
+//  var email = {"email" : $("#email").val()};
+//  if($("#email").val() === ""){
+//      $('.window').show()
+//      .html('<a href="#" class="window_close"></a>' + 'Пожалуйста, введите email!');
+//      e.preventDefault();
+//    }else{
+//      $.ajax({
+//                url:'php/send.php',
+//                type:'POST',
+//                data:'jsonData=' + JSON.stringify(email),
+//                success: function(data) {
+//                    $('.window').show()
+//                        .html('<a href="#" class="window_close"></a>' + data);
+//                        $("#email").val('');
+//                        e.preventDefault();
+//                }
+//            });
+//            return false;
+//    }
+// });
+//  $('.window').on('click', function(e){
+//    e.preventDefault();
+//    $('.window').hide();
+//  });
+
+// ---------------------------------------------------PHP file
+
+// <?php
+//    $data = json_decode($_POST['jsonData']);
+//    $response = ''.count($data).'';
+//    foreach ($data as $key=>$value) {
+//     $response .= ' Email: '.$value.'';
+// }
+// echo $response;
+// ?>
 
 });
